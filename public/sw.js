16e04bc0ad4e5fb12dffee9d2a56027eb0830292
victorcu396 <vmenendezmata@gmail.com>
2026-03-16 14:07:16 +0100

    movil back

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network-first para HTML/JS, Cache-first para assets
 ============================================================ */
 
-const CACHE_NAME = 'kivoospace-v13';
+const CACHE_NAME = 'kivoospace-v14';
 
 // Archivos que se cachean al instalar la PWA
 const PRECACHE_ASSETS = [
