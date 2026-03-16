cce17183bac43662783bcfe41879a5d0f141f854
victorcu396 <vmenendezmata@gmail.com>
2026-03-16 13:20:47 +0100

    downloads corrected

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network-first para HTML/JS, Cache-first para assets
 ============================================================ */
 
-const CACHE_NAME = 'kivoospace-v11';
+const CACHE_NAME = 'kivoospace-v12';
 
 // Archivos que se cachean al instalar la PWA
 const PRECACHE_ASSETS = [
