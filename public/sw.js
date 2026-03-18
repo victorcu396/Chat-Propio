fda6c5e45d07ded0c2ff2ebe9c852c8bfb8d66ab
victorcu396 <vmenendezmata@gmail.com>
2026-03-18 13:48:28 +0100

    sticker&gift supp

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network-first para HTML/JS, Cache-first para assets
 ============================================================ */
 
-const CACHE_NAME = 'kivoospace-v29';
+const CACHE_NAME = 'kivoospace-v30';
 
 // Archivos que se cachean al instalar la PWA
 const PRECACHE_ASSETS = [
