ced1ad18119dd83b349e3c23d9f340e129b78713
victorcu396 <vmenendezmata@gmail.com>
2026-03-20 10:31:09 +0100

    phone number

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network-first para HTML/JS, Cache-first para assets
 ============================================================ */
 
-const CACHE_NAME = 'kivoospace-v54';
+const CACHE_NAME = 'kivoospace-v55';
 
 // Archivos que se cachean al instalar la PWA
 const PRECACHE_ASSETS = [
