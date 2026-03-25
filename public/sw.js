50c17a5e8cc67e0ffad9aa7f562c64d1f4652f66
victorcu396 <vmenendezmata@gmail.com>
2026-03-25 13:13:39 +0100

    menu emojis5

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,7 @@
    Estrategia: Network-first para HTML/JS, Cache-first para assets
 ============================================================ */
 
-const CACHE_NAME = 'kivoospace-v93';
+const CACHE_NAME = 'kivoospace-v94';
 
 // Archivos que se cachean al instalar la PWA
 const PRECACHE_ASSETS = [
